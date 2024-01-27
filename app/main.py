2a28204766fe441d520f7dab01716260fc51c0a5
Libianno <ilibianno@gmail.com>
2024-01-27 02:27:55 -0300

    Montagem do app e finalização da rota

--- app/main.py
+++ app/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from pathlib import Path
 
 from app.routes import home
 
 app = FastAPI()
+
+app.mount(
+        '/static',
+        StaticFiles(directory=Path(__file__).parent.parent.absolute()/'static'),
+        name='static'
+        )
 
 app.include_router(home.router)
 
--- app/routes/home.py
+++ app/routes/home.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
+
 import json
 
 router = APIRouter()
@@ -13,7 +14,7 @@ def home(request: Request, response_class = HTMLResponse):
     with open('animes.json', 'r') as animes:
         animes = animes.read()
         animes = json.loads(animes)
-
-    return templates.TemplateResponse(
-        'home.html', {'request':request}, context=animes
+    
+    return templates.TemplateResponse(request=request,
+        name='home.html', context={'animes': animes}
     )
